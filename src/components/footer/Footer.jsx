bdc47cb4681a17fc81f2acdc20f249b44ad0f9ad
frontend2wd <iqbalkiboo@gmail.com>
2022-06-06 19:22:27 +0700

    [FE] Footer Adding logo Social Media

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../../assets/style/footer.css'
 import LogoICF from '../../assets/icon/logo.svg'
+import facebook from '../../assets/icon/facebook.svg'
+import twitter from '../../assets/icon/twitter.svg'
+import instagram from '../../assets/icon/instagram.svg'
 
 function Footer() {
     return (
@@ -18,6 +21,18 @@ function Footer() {
                     <span>Address: Jakarta Pusat 10270 Indonesia</span>
                     <span> Phone : <a href="tel:+6221 837 925 24">+6221 837 925 24</a></span>
                     <span>Email : <a href="mailto:issi@gmail.com">issi@gmail.com</a></span>
+
+                    <div className="list-right" style={{marginTop: '20px'}}>
+                        <a href="https://www.facebook.com/IndonesianCyclingFederation/">
+                            <img src={facebook} alt="facebook" />
+                        </a>
+                        <a href="https://twitter.com/icf_cycling">
+                            <img src={twitter} alt="twitter" />
+                        </a>
+                        <a href="https://www.instagram.com/icf_cycling/">
+                            <img src={instagram} alt="instagram" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
