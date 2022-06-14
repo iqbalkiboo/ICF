f89fc64ae42de378a4a2838a3da12c2a2df2b9e0
frontend2wd <iqbalkiboo@gmail.com>
2022-06-14 15:29:23 +0700

    [AXIOS] Setting Axios

---
+++ src/service/API/index.js
@@ -0,0 +1,10 @@
+// import Get from "./Get";
+
+// GET
+// NOTE : Gunakan true or false, kalau true dia kirim paramas ke url,
+// kalau false tidak kirim params
+
+const API = {
+};
+
+export default API;
