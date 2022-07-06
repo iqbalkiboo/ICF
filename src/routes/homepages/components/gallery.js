2e7df924b22ce1eb04415adfb06764790d151e40
frontend2wd <iqbalkiboo@gmail.com>
2022-07-06 17:32:58 +0700

    API & responsive

--- src/routes/homepages/components/gallery.js
+++ src/routes/homepages/components/gallery.js
@@ -3,8 +3,8 @@ import '../../../assets/style/gallery.css'
 
 export default function GalleryPages() {
     return (
-        <div class="image-grid">
-          <img class="image-grid-col-2 image-grid-row-2" src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="architecture" />
+        <div className="image-grid">
+          <img className="image-grid-col-2 image-grid-row-2" src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="architecture" />
           <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="architecture"/>
           <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="architecture"/>
           <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="architecture"/>
@@ -12,28 +12,3 @@ export default function GalleryPages() {
         </div>
     );
 }
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast'
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    // cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    // cols: 2,
-  },
-];
--- src/service/API/index.js
+++ src/service/API/index.js
@@ -2,27 +2,34 @@ import Get from "./Get";
 
 // GET
 // NOTE : Gunakan true or false, kalau true dia kirim paramas ke url,
-// /api/news-infos
-// /api/news-details
-// /api/about-uses
 // kalau false tidak kirim params
 
 //home
-const GET_HOME = () => Get("api/homes", "", false);
+const GET_HOME = () => Get("api/homes", "", false)
+const GET_HIGHLIGHT = (params) => Get("api/highlights", params, true)
+const GET_NEWS_ITEM = (params) => Get("api/news-items", params, true)
+const GET_UPCOMING_HOME = (params) => Get("races", params, true)
+const GET_GALLERY_HOME = (params) => Get("api/galleries", params, true)
 
 //news
-const GET_NEWS = () => Get("api/news-infos", "",false);
-const GET_NEWS_DETAIL = () => Get("api/news-details", "",false);
+const GET_NEWS = () => Get("api/news-infos", "",false)
+const GET_NEWS_DETAIL = () => Get("api/news-details", "",false)
 
 //about
-const GET_ABOUT = () => Get("api/about-uses", "", false);
+const GET_ABOUT = () => Get("api/about-uses", "", false)
 
 //live
 const GET_LIVE = (params) =>Get("api/lives", params, true)
 
 
+
+
 const API = {
     GET_HOME,
+    GET_HIGHLIGHT,
+    GET_NEWS_ITEM,
+    GET_UPCOMING_HOME,
+    GET_GALLERY_HOME,
     GET_NEWS,
     GET_NEWS_DETAIL,
     GET_ABOUT,
