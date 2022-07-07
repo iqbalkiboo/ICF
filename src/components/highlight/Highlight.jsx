f13085403be91683db21c1de437bc2507c092ec6
frontend2wd <iqbalkiboo@gmail.com>
2022-07-07 17:49:42 +0700

    highlight upcoming race

--- src/components/highlight/Highlight.jsx
+++ src/components/highlight/Highlight.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import { Grid } from '@mui/material'
 import '../../assets/style/highlight.css'
+import { Carousel } from 'react-responsive-carousel';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import moment from 'moment';
 
 export default function Highlight(props) {
-    const data = props?.props
+    const data = props.props
     return (
         <div className="highlight">
-            {data.map((item, index) => {
-                <Grid key={index} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={3}>
-                        Race Hub
+            <Carousel autoPlay infiniteLoop={true} showThumbs={false}>
+                {data.map(items => (
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        <Grid item xs={3}>
+                            Race Hub
+                        </Grid>
+                        <Grid item xs={1}>
+                            {moment(items?.tgl_dibuka).format('LL')}
+                        </Grid>
+                        <Grid item xs={6}>
+                            <span className="captions">
+                                {items?.nama_event}
+                            </span>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <KeyboardArrowLeft />
+                            <KeyboardArrowRight />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={1}>
-                        {/* {item?.tgl_dibuka} */}
-                    </Grid>
-                    <Grid item xs={6}>
-                        <span className="captions">
-                            {item?.nama_event}
-                        </span>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <KeyboardArrowLeft />
-                        <KeyboardArrowRight />
-                    </Grid>
-                </Grid>
-            })}
+                ))}
+            </Carousel>
         </div>
     )
 }
