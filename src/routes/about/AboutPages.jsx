68645f62289619c24d16d9a1a25786291e2e2926
frontend2wd <iqbalkiboo@gmail.com>
2022-07-11 02:41:04 +0700

    about Responsive

--- src/routes/about/AboutPages.jsx
+++ src/routes/about/AboutPages.jsx
@@ -1,7 +1,9 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import '../../assets/style/about.css'
-import AboutGallery from '../homepages/components/aboutGallery'
+import imageAbout1 from '../../assets/image/about/about1.png'
+import imageAbout2 from '../../assets/image/about/about2.png'
+import imageAbout3 from '../../assets/image/about/about3.png'
 
 export default function AboutPages() {
     return (
@@ -10,8 +12,11 @@ export default function AboutPages() {
                 <div className="labels">About ICF</div>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={6}>
-                        <div>
-                            <AboutGallery />
+                        <div className="image-grid">
+                            <img className="image-grid-col-2 image-grid-row-3" src={itemData[0].img} alt="gallery-icf" />
+                            {itemData?.map((item, index) => (
+                                <img key={index} src={item.img} alt="architecture"/>
+                            ))}
                         </div>
                     </Grid>
                     <Grid item xs={6}>
@@ -52,6 +57,12 @@ export default function AboutPages() {
             <div className="about-pages-2">
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={6}>
+                        <div className="image-grid-hide">
+                            <img className="image-grid-col-2 image-grid-row-3" src={itemData[0].img} alt="gallery-icf" />
+                            {itemData?.map((item, index) => (
+                                <img key={index} src={item.img} alt="architecture"/>
+                            ))}
+                        </div>
                         <div className="sub-years">
                             <div className="years">19<b>89</b></div>
                             <div className="story-about">
@@ -85,8 +96,11 @@ export default function AboutPages() {
                         </div>
                     </Grid>
                     <Grid item xs={6}>
-                        <div>
-                            <AboutGallery />
+                        <div className="image-grid">
+                            <img className="image-grid-col-2 image-grid-row-3" src={itemData[0].img} alt="gallery-icf" />
+                            {itemData?.map((item, index) => (
+                                <img key={index} src={item.img} alt="architecture"/>
+                            ))}
                         </div>
                     </Grid>
                     
@@ -95,8 +109,11 @@ export default function AboutPages() {
             <div className="about-pages">
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={6}>
-                        <div>
-                            <AboutGallery />
+                        <div className="image-grid">
+                            <img className="image-grid-col-2 image-grid-row-3" src={itemData[0].img} alt="gallery-icf" />
+                            {itemData?.map((item, index) => (
+                                <img key={index} src={item.img} alt="architecture"/>
+                            ))}
                         </div>
                     </Grid>
                     <Grid item xs={6}>
@@ -137,3 +154,23 @@ export default function AboutPages() {
         </>
     )
 }
+
+const itemData = [
+    {
+        img: imageAbout1,
+        title: 'story',
+        rows: 3,
+        cols: 2,
+    },
+    {
+        img: imageAbout2,
+        title: 'rodeBike',
+        cols: 2,
+        rows: 2
+    },
+    {
+        img: imageAbout3,
+        title: 'pranko',
+        cols: 2,
+    }
+];
