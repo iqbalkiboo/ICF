c1d141a4a8223e04abe79802e3e244c327a6676d
iqbalkiboo <iqbalkiboo@gmail.com>
2022-07-13 22:51:53 +0700

    Fixed Tab Filter News

--- src/routes/news/News.jsx
+++ src/routes/news/News.jsx
@@ -6,27 +6,19 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
 // components
-import AllPages from './components/all/All.js'
+import NewsSubCategory from './NewsSubCategory.jsx';
 // assets
 import '../../assets/style/news.css'
 
+
 export default function NewsPages() {
-    const [value, setValue] = React.useState('1');
-    const [subCateg, setSubCateg] = React.useState('1')
+    const [value, setValue] = React.useState('1')
     const [tabs, setTabs] = React.useState('ALL')
-    const [tabsCateg, setTabsCateg] = React.useState('ROAD')
     
     const handleChange = (event, newValue) => {
         setTabs(event.target.textContent)
         setValue(newValue);
     };
-
-    const handleSubCateg = (event, newVal) => {
-        setTabsCateg(event.target.textContent)
-        setSubCateg(newVal)
-    }
-
-    console.log(subCateg)
 
     return (
         <div>
@@ -43,20 +35,14 @@ export default function NewsPages() {
                                     <Tab label="PRE EVENT REPORT" value="4" sx={{color: '#fff'}}/>
                                     <Tab label="EVENT REPORT" value="5" sx={{color: '#fff'}}/>
                                 </TabList>
-                                <TabList onChange={handleSubCateg} aria-label="lab API tabs example" >
-                                    <Tab label="ROAD" value="1" sx={{color: '#fff'}}/>
-                                    <Tab label="OFF ROAD" value="2" sx={{color: '#fff'}}/>
-                                    <Tab label="TRACK" value="3" sx={{color: '#fff'}}/>
-                                </TabList>
                             </Box>
-                            <TabPanel value="1" sx={{color: '#fff'}}><AllPages props={tabs} sub={tabsCateg}/></TabPanel>
-                            <TabPanel value="2" sx={{color: '#fff'}}><AllPages props={tabs} sub={tabsCateg}/></TabPanel>
-                            <TabPanel value="3" sx={{color: '#fff'}}><AllPages props={tabs} sub={tabsCateg}/></TabPanel>
-                            <TabPanel value="4" sx={{color: '#fff'}}><AllPages props={tabs} sub={tabsCateg}/></TabPanel>
-                            <TabPanel value="5" sx={{color: '#fff'}}><AllPages props={tabs} sub={tabsCateg}/></TabPanel>
+                            <TabPanel value="1" sx={{color: '#fff'}}><NewsSubCategory props={tabs}/></TabPanel>
+                            <TabPanel value="2" sx={{color: '#fff'}}><NewsSubCategory props={tabs}/></TabPanel>
+                            <TabPanel value="3" sx={{color: '#fff'}}><NewsSubCategory props={tabs}/></TabPanel>
+                            <TabPanel value="4" sx={{color: '#fff'}}><NewsSubCategory props={tabs}/></TabPanel>
+                            <TabPanel value="5" sx={{color: '#fff'}}><NewsSubCategory props={tabs}/></TabPanel>
                         </TabContext>
                     </Box>
-                    {/* bikin baru untuk tab filter subCateg */}
                 </div>
             </div>
         </div>
