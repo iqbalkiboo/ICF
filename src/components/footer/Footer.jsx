f051e8db85de098a10b176af6b674465fda9c7d2
iqbalkiboo <iqbalkiboo@gmail.com>
2022-09-30 00:21:10 +0700

    Fixed: Adding Tiktok for social media

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../assets/style/footer.css'
 import LogoICF from '../../assets/icon/logo.svg'
+import tiktok from '../../assets/icon/tiktok-logo.svg'
 import facebook from '../../assets/icon/facebook.svg'
 import twitter from '../../assets/icon/twitter.svg'
 import instagram from '../../assets/icon/instagram.svg'
@@ -23,6 +24,9 @@ function Footer() {
                     <span>Email : <a href="mailto:issi@gmail.com">issi@gmail.com</a></span>
 
                     <div className="list-right" style={{marginTop: '20px'}}>
+                        <a href="https://vt.tiktok.com/ZSRQECGmV/" rel="noreferrer" target={"_blank"}>
+                            <img src={tiktok} alt="tiktok" style={{marginRight: '12px'}}/>
+                        </a>
                         <a href="https://www.facebook.com/IndonesianCyclingFederation/" rel="noreferrer" target={"_blank"}>
                             <img src={facebook} alt="facebook" />
                         </a>
